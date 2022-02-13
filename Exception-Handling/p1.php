f932e2f6a21305c911b33efed6b9b283998cdcec
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2022-02-13 09:36:13 +0530

    Add files via upload

---
+++ Exception-Handling/p1.php
@@ -0,0 +1,22 @@
+<?php
+
+// wap in php to show Exception Handling
+
+$x=readline('Enter the number 1 :');
+$y=readline('Enter the number 2 :');
+$result=0;
+
+try{
+	echo "Block of try staeted...\n";
+	$result=$x/$y;
+	echo "Block of try Endeded...\n";
+}
+catch(Exception $e){
+	echo "Some Exception Raised".$e->getMessage();
+	echo "\n";
+}
+finally{
+	echo "this block will be , excuted....\n";
+	echo "The Result = $result \n";
+}
+?>
