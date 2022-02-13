b55f5895a00335d36e0bc8c67e75a289a692bc32
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2022-02-13 09:36:43 +0530

    Add files via upload

---
+++ desision control/d1.php
@@ -0,0 +1,13 @@
+<?php
+// wap in php to show normal execution of the program from top to bottom
+$choice=readline("enter the number:");
+echo "Line ".__LINE__." is executing ...\n";
+echo "Line ".__LINE__." is executing ...\n";
+echo "Line ".__LINE__." is executing ...\n";
+echo "Line ".__LINE__." is executing ...\n";
+if($choice>10) {echo "Line ".__LINE__." is executing ...\n";}
+echo "Line ".__LINE__." is executing ...\n";
+echo "Line ".__LINE__." is executing ...\n";
+echo "Line ".__LINE__." is executing ...\n";
+echo "Line ".__LINE__." is executing ...\n";
+?>
