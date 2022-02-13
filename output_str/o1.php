23d4a48ff02048e07fd3119da8279e4e3bb6424e
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2022-02-13 09:27:57 +0530

    Add files via upload

---
+++ output_str/o1.php
@@ -0,0 +1,23 @@
+<?php
+
+//wap in php to show '' inside '' Qoutes is invalid?
+echo PHP_EOL;
+echo 'how are you !!';
+echo PHP_EOL;
+echo "shailehs prajapati";
+echo PHP_EOL; 
+
+echo 'hy, Mr papa1`s friends';     //backtisk
+echo PHP_EOL;
+
+
+#echo 'hy, Mr papa's friends';      //parseError
+
+echo "this is single qoute inside double";
+echo PHP_EOL;
+echo "this is single back qoute`s inside double";
+echo PHP_EOL;
+#echo "Shailesh prajapati"SR prajapati"";
+
+
+?>
---
+++ output_str/o2.php
@@ -0,0 +1,18 @@
+<?php
+
+// wap into show possible no of cases in output string
+
+echo "Hy !! shailesh`prajapati";
+echo PHP_EOL;
+echo '"King is King " SR prajapati';
+echo PHP_EOL;
+echo "hy manish bhai 'shalinee bharti'";
+echo PHP_EOL;
+
+
+echo PHP_EOL;
+echo 'SELECT * from`sonia delhi`;';
+echo PHP_EOL;
+echo "SELECT * from `sultanpur` where ID='1';"; 
+
+?>
